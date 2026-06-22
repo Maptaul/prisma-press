4fefcfc7e6aa2168f5c878795ee0ed054a73d2df
Maptaul <maptaulislam1@gmail.com>
2026-06-22 13:24:30 +0600

    Implement user registration functionality with service and controller

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,7 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
-import { prisma } from "./lib/prisma";
-import httpStatus from "http-status";
-import bcrypt from "bcryptjs";
+import { userRouter } from "./modules/user/user.route";
 
 const app: Application = express();
 
@@ -23,58 +21,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-app.post("/api/users/register", async (req: Request, res: Response) => {
-  const { name, email, password, profilePhoto } = req.body;
-  const isUserExist = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-  if (isUserExist) {
-    return res
-      .status(httpStatus.CONFLICT)
-      .json({ message: "User already exists" });
-  }
-
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcrypt_salt_rounds),
-  );
-
-  const createdUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-  await prisma.profile.create({
-    data: {
-      profilePhoto,
-      userId: createdUser.id,
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      id: createdUser.id,
-      email: createdUser.email,
-    },
-    omit: {
-      password: true,
-    },
-    include: {
-      profile: true,
-    },
-  });
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    statusCode: httpStatus.CREATED,
-    message: "User registered successfully",
-    data: {
-      user,
-    },
-  });
-});
+app.use("/api/users", userRouter);
 
 export default app;
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,55 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import httpStatus from "http-status";
+import config from "../../config";
+import { RegisterUserPayload } from "./user.interface";
+
+const registerUserIntoDB = async (payload: RegisterUserPayload) => {
+  const { name, email, password, profilePhoto } = payload;
+
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (isUserExist) {
+    throw new Error("User already exists with this email");
+  }
+
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  await prisma.profile.create({
+    data: {
+      profilePhoto,
+      userId: createdUser.id,
+    },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: createdUser.id,
+      email: createdUser.email,
+    },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+  return user;
+};
+
+export const userService = {
+  registerUserIntoDB,
+};
