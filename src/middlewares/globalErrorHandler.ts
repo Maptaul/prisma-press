06a7f0ffcbf32c3b32969dfafe49ae1130481539
Maptaul <maptaulislam1@gmail.com>
2026-07-04 12:50:49 +0600

    feat(middlewares): enhance global error handler with detailed error messages

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
+import { Prisma } from "../../generated/prisma/client";
 
 export const globalErrorHandler = (
   err: any,
@@ -7,11 +8,49 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  console.error(err.stack);
+  console.log("Error :", err);
+
+  let statusCode;
+  let errorMessage = err.message || "Internal Server Error";
+  let errorName = err.name || "Internal Server Error";
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    statusCode = httpStatus.BAD_REQUEST;
+    errorMessage =
+      "You have provided invalid data. Please check your request and try again.";
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage = "Duplicate field value entered.";
+    } else if (err.code === "P2003") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage =
+        "Foreign key constraint failed. Please check your request and try again.";
+    } else if (err.code === "P2025") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage =
+        "an operation failed because it depends on one or more records that were required but not found. Please check your request and try again.";
+    }
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      statusCode = httpStatus.UNAUTHORIZED;
+      errorMessage =
+        "Authentication failed. Please check your credentials and try again.";
+    } else if (err.errorCode === "P1001") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage =
+        "Database server is not available. Please try again later.";
+    }
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    errorMessage = "An unknown error occurred. Please try again later.";
+  }
+
   res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
     success: false,
-    statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-    message: err.message,
+    statusCode: statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+    name: errorName,
+    message: errorMessage,
     error: err.stack,
   });
 };
